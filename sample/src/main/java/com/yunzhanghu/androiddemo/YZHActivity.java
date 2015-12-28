aa9e40e74957c68cb7ae743464926ec6828ee37b
sunyujia <sunyujia0227@hotmail.com>
2015-12-28 14:56:58 +0800

    实现打电话的方法,该方法不完美,仍会加载出一个页面,待解决

--- sample/src/main/java/com/yunzhanghu/androiddemo/YZHActivity.java
+++ sample/src/main/java/com/yunzhanghu/androiddemo/YZHActivity.java
@@ -1,12 +1,22 @@
 package com.yunzhanghu.androiddemo;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.yunzhanghu.library.RequestParams;
 import com.yunzhanghu.library.YZHWebViewClient;
@@ -16,6 +26,8 @@ import com.yunzhanghu.library.YZHWebViewClient;
  * Created by max on 15/11/24.
  */
 public class YZHActivity extends Activity {
+
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     private WebView mWebView;
 
@@ -39,11 +51,15 @@ public class YZHActivity extends Activity {
         mWebView.getSettings().setJavaScriptEnabled(true);
         //设置WebView缓存模式
         mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        mWebView.setWebViewClient(new YZHWebViewClient() {
+        mWebView.setWebViewClient(new YZHWebViewClient(this) {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 //保证使用应用内的WebView打开 而不是使用外部浏览器 返回值为true
                 view.loadUrl(url);
+                if (url.startsWith("tel:")){
+                    ringUp(Uri.parse(url));
+                    mWebView.goBackOrForward(-1);
+                }
                 return true;
             }
 
@@ -68,6 +84,7 @@ public class YZHActivity extends Activity {
                     YZHActivity.this.finish();
                 }
             }
+
             @Override
             public void returnBankcard(WebView view, RequestParams requestParams) {
 
@@ -85,4 +102,18 @@ public class YZHActivity extends Activity {
             finish();
         }
     }
+
+    private void ringUp(Uri uri) {
+        int hasCallPhonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+        if (hasCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+                return;
+            }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE_ASK_PERMISSIONS);
+            return;
+        }
+        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+        startActivity(intent);
+    }
+
 }
